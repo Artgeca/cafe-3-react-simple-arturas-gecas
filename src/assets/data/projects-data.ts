e6437a8cd53c82255c4b37eb99a95e22785daf39
artgeca <artgeca@gmail.com>
2022-07-27 22:11:24 +0300

    feat: added projects data

---
+++ src/assets/data/projects-data.ts
@@ -0,0 +1,76 @@
+const projectsData = [
+  {
+    id: 1,
+    type: 'Office',
+    address: '992 Platinum Drive, PA',
+    img: 'https://images.unsplash.com/photo-1554435493-93422e8220c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
+  },
+  {
+    id: 2,
+    type: 'Residential',
+    address: '1543 North Bend River Road, KY',
+    img: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80',
+  },
+  {
+    id: 3,
+    type: 'Office',
+    address: '3675 Junkins Avenue, GA',
+    img: 'https://images.unsplash.com/photo-1574958269340-fa927503f3dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1048&q=80',
+  },
+  {
+    id: 4,
+    type: 'School',
+    address: '1316 Hawks Nest Lane, MO',
+    img: 'https://images.unsplash.com/photo-1592066575517-58df903152f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1598&q=80',
+  },
+  {
+    id: 5,
+    type: 'Hospital',
+    address: '2240 Joseph Street, WI',
+    img: 'https://images.unsplash.com/photo-1626315869436-d6781ba69d6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 6,
+    type: 'Office',
+    address: '3137 Cedar Street, AR',
+    img: 'https://images.unsplash.com/photo-1592401807554-4a13cd4b3225?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 7,
+    type: 'Residential',
+    address: '3675 Junkins Avenue, GA',
+    img: 'https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 8,
+    type: 'Residential',
+    address: '4134 Poplar Chase Lane, ID',
+    img: 'https://images.unsplash.com/photo-1574958269340-fa927503f3dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1048&q=80',
+  },
+  {
+    id: 9,
+    type: 'University',
+    address: '2349 Hickory Ridge Drive, NV',
+    img: 'https://images.unsplash.com/photo-1590579906355-9ea60292515b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2078&q=80',
+  },
+  {
+    id: 10,
+    type: 'Hospital',
+    address: '4952 Gladwell Street, NV',
+    img: 'https://images.unsplash.com/photo-1599902010569-7421fd9d8b0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+  },
+  {
+    id: 11,
+    type: 'Residential',
+    address: '4779 Evergreen Lane, CA',
+    img: 'https://images.unsplash.com/photo-1623298317883-6b70254edf31?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 12,
+    type: 'Office',
+    address: '4432 Broadway Street, SC',
+    img: 'https://images.unsplash.com/photo-1582530239825-1878a59deeb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+];
+
+export default projectsData;
