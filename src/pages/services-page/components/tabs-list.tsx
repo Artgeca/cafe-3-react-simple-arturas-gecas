a81f8adcf2e4244b2970164114b836fb6969039c
artgeca <artgeca@gmail.com>
2022-08-05 09:44:54 +0300

    chore: tab list items data separated and mapped to renderer

--- src/pages/services-page/components/tabs-list.tsx
+++ src/pages/services-page/components/tabs-list.tsx
@@ -5,6 +5,25 @@ interface Props {
   onChange: (e: React.SyntheticEvent, newValue: number) => void
 }
 
+const tabsData = [
+  {
+    label: 'Design',
+    value: 1,
+  },
+  {
+    label: 'Construction',
+    value: 2,
+  },
+  {
+    label: 'Demolition',
+    value: 3,
+  },
+  {
+    label: 'Equipment',
+    value: 4,
+  },
+];
+
 const TabsList = ({ value, onChange }: Props) => (
   <Tabs
     value={value}
@@ -12,10 +31,16 @@ const TabsList = ({ value, onChange }: Props) => (
     variant='scrollable'
     allowScrollButtonsMobile
   >
-    <Tab label='Design' value={1} sx={{ color: 'grey.300' }} />
-    <Tab label='Construction' value={2} sx={{ color: 'grey.300' }} />
-    <Tab label='Demolition' value={3} sx={{ color: 'grey.300' }} />
-    <Tab label='Equipment' value={4} sx={{ color: 'grey.300' }} />
+    {
+      tabsData.map((tab) => (
+        <Tab
+          key={tab.value}
+          label={tab.label}
+          value={tab.value}
+          sx={{ color: 'grey.300', fontSize: 16, fontWeight: { xs: 300, sm: 400 } }}
+        />
+      ))
+    }
   </Tabs>
 );
 
