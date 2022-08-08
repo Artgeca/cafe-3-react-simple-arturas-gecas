f668091eae41c5c929ed130ea14a0dba5f1de367
artgeca <artgeca@gmail.com>
2022-08-08 15:00:35 +0300

    chore: services tab imports fixed

--- src/pages/services-page/components/index.tsx
+++ src/pages/services-page/components/index.tsx
@@ -2,3 +2,5 @@ export { default as DesignTab } from './design-tab';
 export { default as ConstructionTab } from './construction-tab';
 export { default as DemolitionTab } from './demolition-tab';
 export { default as EquipmentTab } from './equipment-tab';
+export { default as TabsList } from './tabs-list';
+export { default as TabPanel } from './tab-panel';
--- src/pages/services-page/index.tsx
+++ src/pages/services-page/index.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import { Box } from '@mui/material';
 import * as Page from '../../components';
 import * as Content from './components';
-import TabPanel from './components/tab-panel';
-import TabsList from './components/tabs-list';
 
 const ServicesPage = () => {
   const [value, setValue] = useState(1);
@@ -20,20 +18,20 @@ const ServicesPage = () => {
       {/* <Page.Title title='Services' /> */}
       <Box sx={{ width: '100%', typography: 'body1' }}>
         <Box display='flex' justifyContent='center' sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabsList value={value} onChange={handleChange} />
+          <Content.TabsList value={value} onChange={handleChange} />
         </Box>
-        <TabPanel value={value} index={1}>
+        <Content.TabPanel value={value} index={1}>
           <Content.DesignTab />
-        </TabPanel>
-        <TabPanel value={value} index={2}>
+        </Content.TabPanel>
+        <Content.TabPanel value={value} index={2}>
           <Content.ConstructionTab />
-        </TabPanel>
-        <TabPanel value={value} index={3}>
+        </Content.TabPanel>
+        <Content.TabPanel value={value} index={3}>
           <Content.DemolitionTab />
-        </TabPanel>
-        <TabPanel value={value} index={4}>
+        </Content.TabPanel>
+        <Content.TabPanel value={value} index={4}>
           <Content.EquipmentTab />
-        </TabPanel>
+        </Content.TabPanel>
       </Box>
     </Page.Content>
   );
