479c04b57eb058e8585f0ba59d02d37213ae4cda
Arturas Gecas <artgeca@gmail.com>
2022-08-23 10:49:16 +0300

    feat: all routes moved to page-routes.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AlertSnackbar, Navbar } from './components';
-import AboutPage from './pages/about-page';
-import ContactsPage from './pages/contacts-page';
-import ErrorPage from './pages/error-page';
-import GaleryPage from './pages/galery-page';
-import HomePage from './pages/home-page';
-import ProjectsPage from './pages/projects-page';
-import ServicesPage from './pages/services-page';
 import theme from './theme';
 import AlertProvider from './contexts/alert-context';
-import StorePage from './pages/store-page';
+import PageRoutes from './routes/page-routes';
 import ServiceNavigationProvider from './contexts/services-page-navigation-context';
-import SignUpPage from './pages/signup-page';
-import LoginPage from './pages/login-page/index';
+import { AlertSnackbar, Navbar } from './components';
 
 const App = () => (
   <BrowserRouter>
@@ -21,22 +12,7 @@ const App = () => (
       <ServiceNavigationProvider>
         <ThemeProvider theme={theme}>
           <Navbar />
-          <Routes>
-            <Route path='/'>
-              <Route index element={<HomePage />} />
-              <Route path='services' element={<ServicesPage />} />
-              <Route path='projects' element={<ProjectsPage />} />
-              <Route path='galery' element={<GaleryPage />} />
-              <Route path='about' element={<AboutPage />} />
-              <Route path='contacts' element={<ContactsPage />} />
-              <Route path='store' element={<StorePage />} />
-              <Route path='auth/'>
-                <Route path='signup' element={<SignUpPage />} />
-                <Route path='login' element={<LoginPage />} />
-              </Route>
-              <Route path='*' element={<ErrorPage />} />
-            </Route>
-          </Routes>
+          <PageRoutes />
           <AlertSnackbar />
         </ThemeProvider>
       </ServiceNavigationProvider>
---
+++ src/routes/page-routes.tsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from '../pages/about-page';
+import ContactsPage from '../pages/contacts-page';
+import ErrorPage from '../pages/error-page';
+import GaleryPage from '../pages/galery-page';
+import HomePage from '../pages/home-page';
+import LoginPage from '../pages/login-page';
+import ProjectsPage from '../pages/projects-page';
+import ServicesPage from '../pages/services-page';
+import SignUpPage from '../pages/signup-page';
+import StorePage from '../pages/store-page';
+
+const PageRoutes: React.FC = () => (
+  <Routes>
+    <Route path='/'>
+      <Route index element={<HomePage />} />
+      <Route path='services' element={<ServicesPage />} />
+      <Route path='projects' element={<ProjectsPage />} />
+      <Route path='galery' element={<GaleryPage />} />
+      <Route path='about' element={<AboutPage />} />
+      <Route path='contacts' element={<ContactsPage />} />
+      <Route path='store' element={<StorePage />} />
+      <Route path='auth/'>
+        <Route path='signup' element={<SignUpPage />} />
+        <Route path='login' element={<LoginPage />} />
+      </Route>
+      <Route path='*' element={<ErrorPage />} />
+    </Route>
+  </Routes>
+);
+
+export default PageRoutes;
