118824120d3721be12e95d983eda9ec90aa44cb9
artgeca <artgeca@gmail.com>
2022-08-31 21:38:45 +0300

    feat: added rentals info to db and rendered sketch to store page

--- src/pages/store-page/index.tsx
+++ src/pages/store-page/index.tsx
@@ -1,12 +1,45 @@
+import { Box, Paper, Typography } from '@mui/material';
+import { useState, useEffect } from 'react';
 import * as Page from '../../components';
 
-const StorePage = () => (
-  <Page.Content sx={{
-    display: 'flex', flexDirection: 'column', alignItems: 'center', pt: { xs: 10, sm: 12 }, px: { xs: 3, md: 5 }, pb: 5, gap: 3,
-  }}
-  >
-    <Page.Title title='StorePage' />
-  </Page.Content>
-);
+const StorePage: React.FC = () => {
+  const [rentals, setRentals] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('http://localhost:8000/rentals?_expand=rentalCategory');
+        const data = await response.json();
+        setRentals([...data]);
+      } catch (error) {
+        console.dir(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <Page.Content sx={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center', pt: { xs: 10, sm: 12 }, px: { xs: 3, md: 5 }, pb: 5, gap: 3,
+    }}
+    >
+      <Page.Title title='Rent equipment' />
+      <Box
+        display='flex'
+        justifyContent='center'
+        flexWrap='wrap'
+        gap={2}
+      >
+        {
+          rentals.map((item) => (
+            <Paper key={item.id} sx={{ p: 3 }}>
+              <Typography>{item.title}</Typography>
+              <Box component='img' src={item.img} />
+            </Paper>
+          ))
+        }
+      </Box>
+    </Page.Content>
+  );
+};
 
 export default StorePage;
