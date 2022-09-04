05fb03f238214d875da8fcacd3a2d2a6f1792818
artgeca <artgeca@gmail.com>
2022-09-04 10:16:35 +0300

    feat: amount field moved to separate component

---
+++ src/components/amount-field/amount-field.tsx
@@ -0,0 +1,33 @@
+import { Box, Button, TextField } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+
+interface Props {
+  amount: number,
+  onInc: () => void,
+  onDec: () => void,
+}
+
+const AmountField: React.FC<Props> = ({ amount, onInc, onDec }) => (
+  <Box display='flex' gap={1}>
+    <Button variant='contained' color='secondary' onClick={onDec}>
+      <RemoveIcon />
+    </Button>
+    <TextField
+      color='secondary'
+      size='small'
+      inputProps={{
+        style: {
+          padding: 0, height: 36, textAlign: 'center',
+        },
+        readOnly: true,
+      }}
+      value={amount}
+    />
+    <Button variant='contained' color='secondary' onClick={onInc}>
+      <AddIcon />
+    </Button>
+  </Box>
+);
+
+export default AmountField;
--- src/pages/store-page/components/rental-card.tsx
+++ src/pages/store-page/components/rental-card.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
-  Paper, Typography, Box, Button, Chip, TextField,
+  Paper, Typography, Box, Button, Chip, TextField, OutlinedInput,
 } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { RentalItem } from '../types';
+import AmountField from '../../../components/amount-field/amount-field';
 
 const RentalCard: React.FC<RentalItem> = ({
   id,
@@ -13,56 +15,53 @@ const RentalCard: React.FC<RentalItem> = ({
   rentalCategory,
   specs,
   img,
-}) => (
-  <Paper sx={{
-    display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between', p: 2, width: 300, height: 520,
-  }}
-  >
-    <Box
-      component='img'
-      src={img}
-      sx={{
-        width: 250,
-        pb: 2,
-      }}
-    />
-    <Box>
-      <Typography variant='h6' fontWeight={400} sx={{ pb: 2 }}>{title}</Typography>
+}) => {
+  const onInc = () => console.log('pressed "+"');
+  const onDec = () => console.log('pressed "-"');
+
+  return (
+    <Paper sx={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-between', p: 2, width: 300, height: 520,
+    }}
+    >
       <Box
-        display='flex'
-        flexDirection='column'
-        gap={1}
-        width='100%'
-        pb={2}
-      >
-        {
-          specs.map((spec) => (
-            <Chip key={spec} label={spec} color='primary' />
-          ))
-        }
-      </Box>
-      <Box sx={{
-        width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2,
-      }}
-      >
-        <Box display='flex' gap={1}>
-          <Button variant='contained' color='secondary'>
-            <RemoveIcon />
-          </Button>
-          <TextField
-            color='secondary'
-            type='number'
-            size='small'
-            fullWidth
-          />
-          <Button variant='contained' color='secondary'>
-            <AddIcon />
-          </Button>
+        component='img'
+        src={img}
+        sx={{
+          width: 250,
+          pb: 2,
+        }}
+      />
+      <Box>
+        <Typography variant='h6' fontWeight={400} sx={{ pb: 2 }}>{title}</Typography>
+        <Box
+          display='flex'
+          flexDirection='column'
+          gap={1}
+          width='100%'
+          pb={2}
+        >
+          {
+            specs.map((spec) => (
+              <Chip key={spec} label={spec} color='primary' />
+            ))
+          }
         </Box>
-        <Button variant='contained' color='secondary' fullWidth>More info</Button>
+        <Box sx={{
+          width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2,
+        }}
+        >
+          <Box display='flex' gap={1}>
+            <AmountField amount={0} onInc={onInc} onDec={onDec} />
+            <Button variant='contained' color='secondary'>
+              <ShoppingCartIcon />
+            </Button>
+          </Box>
+          <Button variant='contained' color='secondary' fullWidth>More info</Button>
+        </Box>
       </Box>
-    </Box>
-  </Paper>
-);
+    </Paper>
+  );
+};
 
 export default RentalCard;
