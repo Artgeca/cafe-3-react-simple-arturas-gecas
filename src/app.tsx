72d051d0c90ef0807e834f7a629d22ba504ad56a
artgeca <artgeca@gmail.com>
2022-10-15 13:53:49 +0300

    feat: created redux store

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 import theme from './theme';
 import AlertProvider from './contexts/alert-context';
 import PageRoutes from './routes/page-routes';
 import ServiceNavigationProvider from './contexts/services-page-navigation-context';
 import { AlertSnackbar, Navbar } from './components';
+import { store } from './store/configure-store';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <AlertProvider>
       <ServiceNavigationProvider>
         <ThemeProvider theme={theme}>
-          <Navbar />
-          <PageRoutes />
-          <AlertSnackbar />
+          <Provider store={store}>
+            <Navbar />
+            <PageRoutes />
+            <AlertSnackbar />
+          </Provider>
         </ThemeProvider>
       </ServiceNavigationProvider>
     </AlertProvider>
