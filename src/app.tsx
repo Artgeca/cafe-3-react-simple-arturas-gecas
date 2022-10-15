c739fc1ffa5c9d92333d84f146de3f4ffa0911cb
artgeca <artgeca@gmail.com>
2022-10-15 13:42:01 +0300

    chore: removed cart context

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,22 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import theme from './theme';
 import AlertProvider from './contexts/alert-context';
-import { CartProvider } from './contexts/cart-context';
 import PageRoutes from './routes/page-routes';
 import ServiceNavigationProvider from './contexts/services-page-navigation-context';
 import { AlertSnackbar, Navbar } from './components';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <LocalizationProvider dateAdapter={AdapterMoment}>
-      <AlertProvider>
-        <ServiceNavigationProvider>
-          <ThemeProvider theme={theme}>
-            <CartProvider>
-              <Navbar />
-              <PageRoutes />
-              <AlertSnackbar />
-            </CartProvider>
-          </ThemeProvider>
-        </ServiceNavigationProvider>
-      </AlertProvider>
-    </LocalizationProvider>
+    <AlertProvider>
+      <ServiceNavigationProvider>
+        <ThemeProvider theme={theme}>
+          <Navbar />
+          <PageRoutes />
+          <AlertSnackbar />
+        </ThemeProvider>
+      </ServiceNavigationProvider>
+    </AlertProvider>
   </BrowserRouter>
 );
 
